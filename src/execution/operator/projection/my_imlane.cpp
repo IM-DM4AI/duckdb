c89b686d7cc6535fb7f773eee4c50150347a4502
lovelynewlife <newlife_1114752@163.com>
2025-08-21 08:32:00 +0000

    build: link imlane dbend library.

---
+++ src/execution/operator/projection/my_imlane.cpp
@@ -0,0 +1,8 @@
+#include "imlane/my_lane.hpp"
+#include "common/arrow_lane.hpp"
+
+namespace mylane {
+    void func() {
+        IMLane::ArrowLaneTable t;
+    }
+}
---
+++ src/include/imlane/my_lane.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "dbend/c/imlane_dbend.hpp"
+
